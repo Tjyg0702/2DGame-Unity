95f1bbcac5b944313c3a27255852569b9acd8a42
Ziyang Deng <zd2014@nyu.edu>
2023-04-21 16:09:55 -0400

    Update PlayerMovement.cs

--- 2D Game/Assets/Script/PlayerMovement.cs
+++ 2D Game/Assets/Script/PlayerMovement.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     private Rigidbody2D rb;
     private BoxCollider2D coll;
     private SpriteRenderer sprite;
     private Animator anim;
 
     Vector2 movement;
-    Vector2 lastMoveDirection = Vector2.zero;
+    Vector2 lastMoveDirection = Vector2.right;
     [SerializeField] private float moveSpeed = 7f;
 
     [SerializeField] private AudioSource jumpSoundEffect;
@@ -31,25 +30,30 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        // Normalize the movement vector
         if (movement != Vector2.zero)
         {
             movement.Normalize();
         }
 
-        anim.SetFloat("Horizontal", movement.x);
+        // Calculate the X difference between the player and the mouse position
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float xDifference = mousePosition.x - transform.position.x;
+
+        // Set the Horizontal animator parameter based on the X difference
+        anim.SetFloat("Horizontal", Mathf.Sign(xDifference));
+
         anim.SetFloat("Vertical", movement.y);
         anim.SetFloat("Speed", movement.sqrMagnitude);
+        
+        // Update lastMoveDirection based on the X difference
+        lastMoveDirection = new Vector2(Mathf.Sign(xDifference), 0);
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-        if (Mathf.Abs(movement.x) > 0.1f || Mathf.Abs(movement.y) > 0.1f)
-        {
-            lastMoveDirection = new Vector2(movement.x, movement.y);
-        }
         anim.SetFloat("LastMoveX", lastMoveDirection.x);
         anim.SetFloat("LastMoveY", lastMoveDirection.y);
     }
-
 }
